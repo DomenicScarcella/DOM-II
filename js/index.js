1345686c42e9876370f2dae9776cf445da9a499f
Domenic Scarcella <ls41513@yahoo.com>
2021-02-09 19:53:59 -0500

    First push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 // Your code goes here
+// BUTTONS
+const navLinks = document.querySelectorAll('.nav a');
+const btnLinks = document.querySelectorAll('.btn');
+
+// IMAGES
+const imgWelcome = document.querySelector('.intro img');
+const imgMiddle = document.querySelectorAll('.img-content img');
+const imgPickDest = document.querySelector('.content-destination img');
+
+const header = document.querySelector('h1')
+const footer = document.querySelector('.footer p');
+
+
+// 1.
+header.addEventListener("mouseenter", function(event) {
+  event.target.style.color = "blue";
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 1000);
+});
+
+// 2.
+document.addEventListener('keydown', function (event) {
+    event.target.style.color = "blue";
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 1000);
+});
+
+// 3.
+function zoom(event) {
+    event.preventDefault();
+    scale += event.deltaY * -0.002;
+    scale = Math.min(Math.max(.25, scale), 1.75);
+    el.style.transform = `scale(${scale})`;
+  }
+  let scale = 1;
+  const el = document.querySelector('footer');
+  el.onwheel = zoom;
+
+// 4.
+
+  
+
