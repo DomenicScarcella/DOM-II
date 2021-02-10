70e4d190c9609bd61d486a945ee03e968a1c4cc2
Domenic Scarcella <ls41513@yahoo.com>
2021-02-09 23:45:43 -0500

    All except 'Nest . . . '

--- js/index.js
+++ js/index.js
@@ -9,26 +9,77 @@ const imgMiddle = document.querySelectorAll('.img-content img');
 const imgPickDest = document.querySelector('.content-destination img');
 
 const header = document.querySelector('h1')
+const body = document.querySelector('body')
 const footer = document.querySelector('.footer p');
 
 
 // 1.
-header.addEventListener("mouseenter", function(event) {
-  event.target.style.color = "blue";
+body.addEventListener('mouseenter', function(event) {
+  event.target.style.color = 'blue';
   setTimeout(function() {
-    event.target.style.color = "";
+    event.target.style.color = '';
   }, 1000);
 });
 
 // 2.
-document.addEventListener('keydown', function (event) {
-    event.target.style.color = "blue";
+body.addEventListener('keydown', function (event) {
+  event.target.style.color = 'blue';
   setTimeout(function() {
-    event.target.style.color = "";
+    event.target.style.color = '';
   }, 1000);
 });
 
 // 3.
+body.addEventListener('keypress', function (event) {
+    event.target.style.color = 'blue';
+  });
+  
+// 4.
+body.addEventListener('keyup', function (event) {
+  event.target.style.color = 'yellow';
+  setTimeout(function() {
+    event.target.style.color = '';
+  }, 3000);
+});
+
+// 5.
+body.addEventListener('dblclick', function(event) {
+  event.target.style.color = 'blue';
+  setTimeout(function() {
+    event.target.style.color = '';
+  }, 1000);
+});
+
+// 6.
+body.addEventListener('contextmenu', function(event) {
+  event.target.style.color = 'blue';
+  setTimeout(function() {
+    event.target.style.color = '';
+  }, 1000);
+});
+
+// 7.
+body.addEventListener('mouseout', function(event) {
+  event.target.style.color = 'blue';
+  setTimeout(function() {
+    event.target.style.color = '';
+  }, 1000);
+});
+
+// 8.
+body.addEventListener('mouseup', function(event) {
+  event.target.style.color = 'blue';
+  setTimeout(function() {
+    event.target.style.color = '';
+  }, 1000);
+});
+
+// 9.
+body.addEventListener('copy', function (event) {
+    alert('Ctrl + C makes text blue');
+})
+
+// 10.
 function zoom(event) {
     event.preventDefault();
     scale += event.deltaY * -0.002;
@@ -39,7 +90,10 @@ function zoom(event) {
   const el = document.querySelector('footer');
   el.onwheel = zoom;
 
-// 4.
 
-  
-
+// preventDefault
+Array.from(document.links).forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault()
+    })
+})
